178d5566d8cbb3b0dcd108be8ba9e4e974cf8fe7
Petr Pivoňka <petrpiv1@gmail.com>
2020-11-26 14:22:19 +0100

    Fix ground remove for TMUF

--- NationsConverter/Stages/DefaultGroundRemover.cs
+++ NationsConverter/Stages/DefaultGroundRemover.cs
@@ -12,11 +12,14 @@ namespace NationsConverter.Stages
         {
             map.ImportFileToEmbed("UserData/Blocks/NationsConverter/NoGround.Block.Gbx", "Blocks/NationsConverter");
 
-            for (var x = 0; x < map.Size.GetValueOrDefault().X; x++)
+            for (var x = 1; x <= map.Size.GetValueOrDefault().X; x++)
             {
-                for (var z = 0; z < map.Size.GetValueOrDefault().Z; z++)
+                for (var z = 1; z <= map.Size.GetValueOrDefault().Z; z++)
                 {
-                    map.Blocks.Add(new CGameCtnBlock(@"NationsConverter\NoGround.Block.Gbx_CustomBlock", Direction.North, (x, 0, z)) { IsGround = true });
+                    Int3 coord = (x, 0, z);
+                    if (version >= GameVersion.TM2)
+                        coord += parameters.Stadium2RelativeOffsetCoord.XZ;
+                    map.Blocks.Add(new CGameCtnBlock(@"NationsConverter\NoGround.Block.Gbx_CustomBlock", Direction.North, coord) { IsGround = true });
                 }
             }
         }
