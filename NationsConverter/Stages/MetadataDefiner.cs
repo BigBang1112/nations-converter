d04ebd5df43f803796522732294afe9f758693f3
Petr Pivoňka <petrpiv1@gmail.com>
2020-12-06 15:37:11 +0100

    Add GBX.NET assembly metadata

--- NationsConverter/Stages/MetadataDefiner.cs
+++ NationsConverter/Stages/MetadataDefiner.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using System.Configuration;
+using System.Linq;
 
 namespace NationsConverter.Stages
 {
@@ -12,10 +13,12 @@ namespace NationsConverter.Stages
         public void Process(CGameCtnChallenge map, int version, ConverterParameters parameters)
         {
             var assembly = Assembly.GetEntryAssembly();
+            var assemblyGBXNET = assembly.GetReferencedAssemblies().FirstOrDefault(x => x.Name == "GBX.NET");
 
             map.CreateChunk<CGameCtnChallenge.Chunk03043044>();
             map.ScriptMetadata.Declare("MadeWithNationsConverter", true);
             map.ScriptMetadata.Declare("NC_Assembly", assembly.FullName);
+            map.ScriptMetadata.Declare("NC_GBXNET_Assembly", assemblyGBXNET.FullName);
             map.ScriptMetadata.Declare("NC_EarlyAccess", true);
         }
     }
