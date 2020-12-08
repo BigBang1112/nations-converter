9a7437c2b4e73f80390e21a2a86bd1cc25337bb1
Petr Pivoňka <petrpiv1@gmail.com>
2020-12-09 00:50:04 +0100

    GUI changes

---
+++ NationsConverterGUI/ListViewMapItem.cs
@@ -0,0 +1,37 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace NationsConverterGUI
+{
+    public class ListViewMapItem
+    {
+        public string FileName => Path.GetFileName(GBX?.FileName);
+        public BitmapImage Thumbnail { get; set; }
+        public GameBox<CGameCtnChallenge> GBX { get; set; }
+
+        public ListViewMapItem(GameBox<CGameCtnChallenge> gbx)
+        {
+            GBX = gbx;
+
+            Thumbnail = new BitmapImage();
+
+            using (var ms = new MemoryStream())
+            {
+                gbx.MainNode.Thumbnail.Result.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                Thumbnail.BeginInit();
+                Thumbnail.StreamSource = ms;
+                Thumbnail.CacheOption = BitmapCacheOption.OnLoad;
+                Thumbnail.DecodePixelWidth = 128;
+                Thumbnail.EndInit();
+            }
+        }
+    }
+}
--- NationsConverterGUI/MainWindow.xaml.cs
+++ NationsConverterGUI/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,85 @@ namespace NationsConverterGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<ListViewMapItem> Maps { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            Maps = new ObservableCollection<ListViewMapItem>();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e) => Close();
+        private void buttonMinimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void buttonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private async void listViewMaps_Drop(object sender, DragEventArgs e)
+        {
+            var formats = e.Data.GetFormats();
+
+            if (formats.Contains("FileDrop"))
+            {
+                var fileNames = (string[])e.Data.GetData("FileDrop");
+
+                List<Task<GameBox<CGameCtnChallenge>>> loadingMaps = new List<Task<GameBox<CGameCtnChallenge>>>();
+                
+                foreach (var file in fileNames)
+                {
+                    loadingMaps.Add(Task.Run(() =>
+                    {
+                        try
+                        {
+                            var gbx = GameBox.Parse(file);
+
+                            if (gbx != null)
+                            {
+                                if (gbx is GameBox<CGameCtnChallenge> gbxMap)
+                                {
+                                    if (gbxMap.MainNode.Collection == "Stadium")
+                                    {
+                                        listViewMaps.Dispatcher.Invoke(() => Maps.Add(new ListViewMapItem(gbxMap)));
+                                        return gbxMap;
+                                    }
+                                }
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+
+                        return null;
+                    }));
+                }
+
+                await Task.WhenAll(loadingMaps);
+            }
+        }
+
+        private void listViewMaps_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                while (listViewMaps.SelectedIndex > -1)
+                {
+                    Maps.RemoveAt(listViewMaps.SelectedIndex);
+                }
+            }
         }
     }
 }
