ea84947fc46a70192ad1fcf545a2c1ddd82bdf86
Petr Pivoňka <petrpiv1@gmail.com>
2024-05-04 05:21:00 +0200

    Init v2

---
+++ NationsConverterBot/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace NationsConverterBot;
+
+internal sealed class Startup : IHostedService
+{
+    private readonly IDiscordBot _bot;
+    private readonly ILogger<Startup> _logger;
+
+    public Startup(IDiscordBot bot, ILogger<Startup> logger)
+    {
+        _bot = bot;
+        _logger = logger;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Starting bot...");
+
+        await _bot.StartAsync();
+
+        // ... further startup logic here ...
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _bot.StopAsync();
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Nations Converter
-
-[![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/BigBang1112/nations-converter?include_prereleases&style=for-the-badge)](https://github.com/BigBang1112/nations-converter/releases)
-[![GitHub all releases](https://img.shields.io/github/downloads/BigBang1112/nations-converter/total?style=for-the-badge)](https://github.com/BigBang1112/nations-converter/releases)
-
-Powered by [GBX.NET](https://github.com/BigBang1112/gbx-net).
-
-We are coming back soon.
-
-**Current Nations Converter maps crash the game. Do not use v1.**
+# NationsConverter
