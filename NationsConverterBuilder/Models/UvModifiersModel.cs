3ed143588b5da82c89764de62a793371416f59d3
Petr Pivoňka <petrpiv1@gmail.com>
2024-06-06 02:27:44 +0200

    Add ScaleX and ScaleY

--- NationsConverterBuilder/Models/UvModifiersModel.cs
+++ NationsConverterBuilder/Models/UvModifiersModel.cs
@@ -4,4 +4,6 @@ internal sealed class UvModifiersModel
 {
     public float Rotate { get; set; }
     public float Scale { get; set; } = 1;
+    public float ScaleX { get; set; } = 1;
+    public float ScaleY { get; set; } = 1;
 }
--- NationsConverterBuilder/Services/UvModifierService.cs
+++ NationsConverterBuilder/Services/UvModifierService.cs
@@ -21,6 +21,32 @@ internal sealed class UvModifierService
             }
         }
 
+        if (uvModifiers.ScaleX != 1)
+        {
+            foreach (var texSet in visual.TexCoords)
+            {
+                texSet.TexCoords = texSet.TexCoords.Select(texCoord =>
+                {
+                    var x = texCoord.UV.X - 0.5f;
+                    var y = texCoord.UV.Y - 0.5f;
+                    return texCoord with { UV = new Vec2(x * uvModifiers.ScaleX + 0.5f, y + 0.5f) };
+                }).ToArray();
+            }
+        }
+
+        if (uvModifiers.ScaleY != 1)
+        {
+            foreach (var texSet in visual.TexCoords)
+            {
+                texSet.TexCoords = texSet.TexCoords.Select(texCoord =>
+                {
+                    var x = texCoord.UV.X - 0.5f;
+                    var y = texCoord.UV.Y - 0.5f;
+                    return texCoord with { UV = new Vec2(x + 0.5f, y * uvModifiers.ScaleY + 0.5f) };
+                }).ToArray();
+            }
+        }
+
         if (uvModifiers.Rotate != 0)
         {
             foreach (var texSet in visual.TexCoords)
