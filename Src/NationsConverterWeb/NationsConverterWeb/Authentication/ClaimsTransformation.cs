35c5556c5c0131c750dcb8ac533179c511a77f7b
Petr Pivoňka <petrpiv1@gmail.com>
2024-08-25 05:27:55 +0200

    Add Discord authentication to website

---
+++ Src/NationsConverterWeb/NationsConverterWeb/Authentication/ClaimsTransformation.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace NationsConverterWeb.Authentication;
+
+public class ClaimsTransformation : IClaimsTransformation
+{
+    private readonly IConfiguration config;
+
+    public ClaimsTransformation(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        if (principal.Identity is not ClaimsIdentity { IsAuthenticated: true } identity)
+        {
+            return Task.FromResult(principal);
+        }
+
+        var snowflake = identity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (snowflake is null)
+        {
+            return Task.FromResult(principal);
+        }
+
+        var admins = config.GetSection("Discord:Admins").Get<HashSet<string>>();
+
+        if (admins?.Contains(snowflake) == true)
+        {
+            identity.AddClaim(new(ClaimTypes.Role, "Admin"));
+        }
+
+        var developers = config.GetSection("Discord:Developers").Get<HashSet<string>>();
+
+        if (developers?.Contains(snowflake) == true)
+        {
+            identity.AddClaim(new(ClaimTypes.Role, "Developer"));
+        }
+
+        var modelers = config.GetSection("Discord:Modelers").Get<HashSet<string>>();
+
+        if (modelers?.Contains(snowflake) == true)
+        {
+            identity.AddClaim(new(ClaimTypes.Role, "Modeler"));
+        }
+
+        return Task.FromResult(principal);
+    }
+}
--- Src/NationsConverterWeb/NationsConverterWeb/Program.cs
+++ Src/NationsConverterWeb/NationsConverterWeb/Program.cs
@@ -1,7 +1,51 @@
-using NationsConverterWeb.Client.Pages;
+using AspNet.Security.OAuth.Discord;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.ResponseCompression;
+using NationsConverterWeb.Authentication;
 using NationsConverterWeb.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/";
+        options.AccessDeniedPath = "/thank-you";
+    })
+    .AddDiscord(options =>
+    {
+        options.ClientId = builder.Configuration["Discord:ClientId"] ?? throw new InvalidOperationException("Discord ClientId is missing");
+        options.ClientSecret = builder.Configuration["Discord:ClientSecret"] ?? throw new InvalidOperationException("Discord ClientSecret is missing");
+        options.AccessDeniedPath = "/";
+        options.ClaimActions.MapJsonKey(DiscordAdditionalClaims.GlobalName, "global_name");
+
+        options.Events.OnCreatingTicket = async context =>
+        {
+            // add user to database
+        };
+    });
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+});
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
+builder.Services.AddTransient<IClaimsTransformation, ClaimsTransformation>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -9,6 +53,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,7 +70,24 @@ else
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+}
+
 app.UseStaticFiles();
+
+app.MapGet("/login-discord", async (HttpContext context) =>
+{
+    await context.ChallengeAsync(DiscordAuthenticationDefaults.AuthenticationScheme, new()
+    {
+        RedirectUri = "/dashboard"
+    });
+});
+
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
