0ce1fca49ba1e96fdd635ff169c23971d9553e9f
Petr Pivoňka <petrpiv1@gmail.com>
2024-11-07 00:17:11 +0100

    Offset camera custom and camera path

--- Src/NationsConverter/Stages/MediaTrackerStage.cs
+++ Src/NationsConverter/Stages/MediaTrackerStage.cs
@@ -1,4 +1,5 @@
-﻿using GBX.NET.Engines.Game;
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
 
 namespace NationsConverter.Stages;
 
@@ -8,11 +9,17 @@ internal sealed class MediaTrackerStage
     private readonly CGameCtnChallenge mapOut;
     private readonly NationsConverterConfig config;
 
+    private readonly Int3 blockSize;
+    private readonly Int3 centerOffset;
+
     public MediaTrackerStage(CGameCtnChallenge mapIn, CGameCtnChallenge mapOut, NationsConverterConfig config)
     {
         this.mapIn = mapIn;
         this.mapOut = mapOut;
         this.config = config;
+
+        blockSize = mapIn.Collection.GetValueOrDefault().GetBlockSize();
+        centerOffset = new Int3((mapOut.Size.X - mapIn.Size.X) / 2, 0, (mapOut.Size.Z - mapIn.Size.Z) / 2) ;
     }
 
     public void Convert()
@@ -23,12 +30,62 @@ internal sealed class MediaTrackerStage
         }
 
         // TODO
-        // Align cameras
+        // !!! Recreate media tracker from scratch, as this way its mutating
         // Update to modern data chunks in cases where it crashes
+        OffsetMediaTracker(mapIn.ClipIntro);
+        OffsetMediaTracker(mapIn.ClipGroupInGame);
+        OffsetMediaTracker(mapIn.ClipGroupEndRace);
+        OffsetMediaTracker(mapIn.ClipAmbiance);
+
 
         mapOut.ClipIntro = mapIn.ClipIntro;
         mapOut.ClipGroupInGame = mapIn.ClipGroupInGame;
         mapOut.ClipGroupEndRace = mapIn.ClipGroupEndRace;
         mapOut.ClipAmbiance = mapIn.ClipAmbiance;
+    }
+
+    private void OffsetMediaTracker(CGameCtnMediaClip? clip)
+    {
+        if (clip is null)
+        {
+            return;
+        }
+
+        foreach (var track in clip.Tracks)
+        {
+            foreach (var block in track.Blocks)
+            {
+                if (block is CGameCtnMediaBlockCameraCustom { Keys: not null } cameraCustom)
+                {
+                    foreach (var key in cameraCustom.Keys)
+                    {
+                        key.Position += centerOffset * blockSize;
+                    }
+                    continue;
+                }
+
+                if (block is CGameCtnMediaBlockCameraPath { Keys: not null } cameraPath)
+                {
+                    foreach (var key in cameraPath.Keys)
+                    {
+                        key.Position += centerOffset * blockSize;
+                    }
+                    continue;
+                }
+            }
+        }
+    }
+
+    private void OffsetMediaTracker(CGameCtnMediaClipGroup? clipGroup)
+    {
+        if (clipGroup is null)
+        {
+            return;
+        }
+
+        foreach (var (clip, _) in clipGroup.Clips)
+        {
+            OffsetMediaTracker(clip);
+        }
     }
 }
