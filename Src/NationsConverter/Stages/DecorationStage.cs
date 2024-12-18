f2972924650a18da9bf1fab45e9e0aa3b7ae29e8
Petr Pivoňka <petrpiv1@gmail.com>
2024-12-18 15:14:24 +0100

    Add smooth edges to Stadium

--- Src/NationsConverter/Stages/DecorationStage.cs
+++ Src/NationsConverter/Stages/DecorationStage.cs
@@ -14,7 +14,7 @@ internal sealed partial class DecorationStage : EnvironmentStageBase
     private readonly CustomContentManager customContentManager;
     private readonly ILogger logger;
 
-    private readonly bool includeDecoration;
+    private readonly bool includeDecorationItem;
 
     [GeneratedRegex(@"(Sunrise|Day|Sunset|Night)", RegexOptions.IgnoreCase)]
     private static partial Regex MoodRegex();
@@ -33,7 +33,7 @@ internal sealed partial class DecorationStage : EnvironmentStageBase
         this.customContentManager = customContentManager;
         this.logger = logger;
 
-        includeDecoration = config.IncludeDecoration && Environment != "Stadium";
+        includeDecorationItem = config.IncludeDecoration && Environment != "Stadium";
     }
 
     public void Convert()
@@ -47,11 +47,11 @@ internal sealed partial class DecorationStage : EnvironmentStageBase
 
         mood = string.Concat(mood[0].ToString().ToUpper(), mood.AsSpan(1));
 
-        var mapBase = includeDecoration
+        var mapBase = includeDecorationItem
             ? "NoStadium48x48"
             : "48x48Screen155";
 
-        if (includeDecoration)
+        if (includeDecorationItem)
         {
             var blockSize = mapIn.Collection.GetValueOrDefault().GetBlockSize();
             mapOut.Size = new((int)(mapIn.Size.X * (blockSize.X / 32f)), 40, (int)(mapIn.Size.Z * (blockSize.Z / 32f)));
@@ -62,7 +62,7 @@ internal sealed partial class DecorationStage : EnvironmentStageBase
         logger.LogInformation("Decoration: {Name}", mapOut.Decoration.Id);
         logger.LogInformation("Size: {Size}", mapOut.Size);
 
-        if (includeDecoration)
+        if (includeDecorationItem)
         {
             var sizeStr = $"{mapIn.Size.X}x{mapIn.Size.Y}x{mapIn.Size.Z}";
             var itemPath = Path.Combine("Decorations", $"{sizeStr}.Item.Gbx");
@@ -78,18 +78,145 @@ internal sealed partial class DecorationStage : EnvironmentStageBase
             logger.LogInformation("Placed decoration item ({Size}).", sizeStr);
         }
 
-        var size = includeDecoration ? mapOut.Size : mapIn.Size;
-        var offset = new Int3((mapOut.Size.X - mapIn.Size.X) / 2, 0, (mapOut.Size.Z - mapIn.Size.Z) / 2);
+        var voidSize = includeDecorationItem ? mapOut.Size : mapIn.Size;
 
-        for (var x = 0; x < size.X; x++)
+        // if Stadium, expand void size to be under the height transition
+
+        var voidOffset = new Int3((mapOut.Size.X - mapIn.Size.X) / 2, 0, (mapOut.Size.Z - mapIn.Size.Z) / 2);
+
+        for (var x = 0; x < voidSize.X; x++)
         {
-            for (var z = 0; z < size.Z; z++)
+            for (var z = 0; z < voidSize.Z; z++)
             {
-                var block = customContentManager.PlaceBlock(@"Misc\Void", (x, 9, z) + offset, Direction.North, isGround: true);
-                block.LightmapQuality = LightmapQuality.Lowest;
+                PlaceVoidBlock(voidOffset.X + x, voidOffset.Z + z);
             }
         }
 
-        logger.LogInformation("Placed {SizeX}x{SizeZ} void.", size.X, size.Z);
+        logger.LogInformation("Placed {SizeX}x{SizeZ} void.", voidSize.X, voidSize.Z);
+
+        if (Environment == "Stadium")
+        {
+            PlaceTransitionGrass();
+            PlaceTransitionVoid();
+        }
+    }
+
+    private void PlaceVoidBlock(int x, int z)
+    {
+        var block = customContentManager.PlaceBlock(@"Misc\Void", (x, 9, z), Direction.North, isGround: true);
+        block.LightmapQuality = LightmapQuality.Lowest;
+    }
+
+    private void PlaceTransitionGrass()
+    {
+        const string grassCorner = @"Misc\Border\GrassCorner.Item.Gbx";
+        const string grassEdge = @"Misc\Border\GrassEdge.Item.Gbx";
+
+        var mapSize = mapIn.Size;
+        var offset = new Int3(224, 8, 224);
+        var blockSize = new Int3(32, 8, 32);
+
+        customContentManager.PlaceItem(grassCorner, offset, default);
+
+        for (var x = 0; x < mapSize.X; x++)
+        {
+            customContentManager.PlaceItem(grassEdge, offset + ((x + 1) * blockSize.X, 0, 0), default);
+        }
+
+        customContentManager.PlaceItem(grassCorner, offset + ((mapSize.X + 2) * blockSize.X, 0, 0), (AdditionalMath.ToRadians(-90), 0, 0));
+
+        for (var z = 0; z < mapSize.Z; z++)
+        {
+            customContentManager.PlaceItem(grassEdge, offset + ((mapSize.X + 2) * blockSize.X, 0, (z + 1) * blockSize.Z), (AdditionalMath.ToRadians(-90), 0, 0));
+        }
+
+        customContentManager.PlaceItem(grassCorner, offset + ((mapSize.X + 2) * blockSize.X, 0, (mapSize.Z + 2) * blockSize.Z), (AdditionalMath.ToRadians(180), 0, 0));
+
+        for (var x = 0; x < mapSize.X; x++)
+        {
+            customContentManager.PlaceItem(grassEdge, offset + ((mapSize.X + 1) * blockSize.X - x * blockSize.X, 0, (mapSize.Z + 2) * blockSize.Z), (AdditionalMath.ToRadians(180), 0, 0));
+        }
+
+        customContentManager.PlaceItem(grassCorner, offset + (0, 0, (mapSize.Z + 2) * blockSize.Z), (AdditionalMath.ToRadians(90), 0, 0));
+
+        for (var z = 0; z < mapSize.Z; z++)
+        {
+            customContentManager.PlaceItem(grassEdge, offset + (0, 0, (mapSize.Z + 1) * blockSize.Z - z * blockSize.Z), (AdditionalMath.ToRadians(90), 0, 0));
+        }
+    }
+
+    private void PlaceTransitionVoid()
+    {
+        var voidLength = 8;
+
+        var mapSize = mapIn.Size;
+
+        // Edges
+
+        for (var x = 0; x < mapSize.X; x++)
+        {
+            for (var z = 0; z < voidLength; z++)
+            {
+                PlaceVoidBlock(voidLength + x, z);
+            }
+        }
+
+        for (var x = 0; x < voidLength; x++)
+        {
+            for (var z = 0; z < mapSize.Z; z++)
+            {
+                PlaceVoidBlock(x + mapSize.X + voidLength, voidLength + z);
+            }
+        }
+
+        for (var x = 0; x < mapSize.X; x++)
+        {
+            for (var z = 0; z < voidLength; z++)
+            {
+                PlaceVoidBlock(x + voidLength, z + mapSize.Z + voidLength);
+            }
+        }
+
+        for (var x = 0; x < voidLength; x++)
+        {
+            for (var z = 0; z < mapSize.Z; z++)
+            {
+                PlaceVoidBlock(x, z + voidLength);
+            }
+        }
+
+        // Corners
+
+        for (var x = 0; x < voidLength; x++)
+        {
+            for (var z = 0; z < voidLength; z++)
+            {
+                PlaceVoidBlock(x, voidLength - z - 1);
+            }
+        }
+
+        for (var x = 0; x < voidLength; x++)
+        {
+            for (var z = 0; z < voidLength; z++)
+            {
+                PlaceVoidBlock(x + mapSize.X + voidLength, z);
+            }
+        }
+
+        for (var x = 0; x < voidLength; x++)
+        {
+            for (var z = 0; z < voidLength; z++)
+            {
+                PlaceVoidBlock(x + mapSize.X + voidLength, z + mapSize.Z + voidLength);
+            }
+        }
+
+        for (var x = 0; x < voidLength; x++)
+        {
+            for (var z = 0; z < voidLength; z++)
+            {
+                PlaceVoidBlock(x, z + mapSize.Z + voidLength);
+            }
+        }
     }
 }
