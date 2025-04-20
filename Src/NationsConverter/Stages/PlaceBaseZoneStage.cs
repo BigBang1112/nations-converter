013337499022864e0751bf8ab4cfc8c202bba29e
Petr Pivoňka <petrpiv1@gmail.com>
2025-04-20 04:21:05 +0200

    Fix ghost placement

--- Src/NationsConverter/Stages/PlaceBaseZoneStage.cs
+++ Src/NationsConverter/Stages/PlaceBaseZoneStage.cs
@@ -39,7 +39,8 @@ internal sealed class PlaceBaseZoneStage : BlockStageBase
 
     protected override void ConvertBlock(CGameCtnBlock block, ManualConversionModel conversion)
     {
-        if (block.IsGhost)
+        // tm2 thing where ghost blocks of air blocks are used in a ground, so that the base zone is not removed from that
+        if (conversion.Ground is null && block.IsGhost)
         {
             return;
         }
